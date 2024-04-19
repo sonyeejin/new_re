d8d5a55b54104893ad66c286ea3281fda43d1713
손예진 <son-yejin@yejin-mac.local>
2024-04-19 15:58:47 +0900

    6

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
+import requests
+
 app = FastAPI()
 
 @app.get("/")
 def root():
+    URL = "http://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=12&apiKey=0A1vJmnneiyX5MA432Vw0ZNicz9NOLcICnnpuYau0A1vJmnneiyX5MA432Vw0ZNicz9NOLcICnnpuYau&returnType=json"
+
+    contents = requests.get(URL).text
+
     return { "message": "Hello Root!" }
 
 @app.get("/home")
